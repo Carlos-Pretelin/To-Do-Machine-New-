e587e34f461b6aef8b6be120745302c6490b327a
Carlos Pretelin <carlosppretelin@gmail.com>
2023-04-03 12:08:32 -0600

    Basic project structure done, all the main components are added to the App component

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+//components
+import TodoCounter from "./TodoCounter"
+import TodoSearch from "./TodoSearch"
+import TodoList from "./TodoList"
+import TodoItem from "./TodoItem"
+import CreateTodoButton from "./CreateTodoButton"
+//scss
+import "../styles/App.scss"
+
+const App = () => {
+
+  const todos = [
+    {text: "Hola mundo", completed: false},
+    {text: "Hola Amigo", completed: false},
+    {text: "Hola a Todos", completed: false}
+  ]
+
+
+
+
+
+  return (
+    <>
+      <TodoCounter/>
+
+      <TodoSearch/> 
+      <input type="text" placeholder='Cebolla'/>
+
+      <TodoList>
+        {todos.map ( todo => (
+          <TodoItem todo={todo} key={todo.text}/>
+        ))}
+      </TodoList>
+
+      <CreateTodoButton/>
+      <button>+</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TodoCounter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoCounter = () => {
+  return (
+    <h2>Has completado X de X To-dos</h2>
+  )
+}
+
+export default TodoCounter
